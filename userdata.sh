031e5670faa5b2c9d2e993528415647b80180706
Yoshinori Teraoka <teraoka.yoshinori@gmail.com>
2019-03-31 10:31:02 +0900

    root privilege is not required

--- userdata.sh
+++ userdata.sh
@@ -3,9 +3,6 @@
 curl https://releases.rancher.com/install-docker/18.09.sh | sh
 
 useradd -G docker rancher
-echo "rancher ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/rancher
-chown root:root /etc/sudoers.d/rancher
-chmod 0440 /etc/sudoers.d/rancher
 install -o rancher -g rancher -m 0700 -d ~rancher/.ssh
 cp -p ~root/.ssh/authorized_keys ~rancher/.ssh/authorized_keys
 chown rancher:rancher ~rancher/.ssh/authorized_keys
